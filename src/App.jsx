6f0f63fd225269b396f4e7792b984b42f88352c9
bachkhoa <bachkhoa1988us@gmail.com>
2026-09-19 23:37:07 -0400

    5.5 xuất và nhập component theo tên

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,19 @@ import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
-function Welcome() {
-  //tham so
-  return (
-    <>
-      <h3>jacksonville</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus autem quasi quo! Cupiditate vero sunt vitae non totam quod dolor rem ad quaerat, laboriosam eligendi eaque. Nesciunt, quo? Sequi, nihil?</p>
-    </>
-  )
-}
+import { Headeṛ, Sidebar, Footer } from './components/5.5 - Xuất và nhập component theo tên'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>chào mừng bạn đén vói react</h1>
-      <Welcome />
+      < Headeṛ />
+      < Sidebar />
+      < Footer />
     </>
+
   )
 }
 
---
+++ src/components/5.5 - Xuất và nhập component theo tên.jsx
@@ -0,0 +1,10 @@
+function Headeṛ() {
+    return <h1>Đây là header</h1>;
+}
+function Sidebar() {
+    return <h1>Đây là Sidebar</h1>;
+}
+function Footer() {
+    return <h1>Đây là Footer</h1>;
+}
+export { Headeṛ, Sidebar, Footer }
