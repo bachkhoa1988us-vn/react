abbe2cff03bfb143725ec355dd515f09adf32a42
bachkhoa <bachkhoa1988us@gmail.com>
2026-09-20 00:38:50 -0400

    5.6 - 3 quy tắc tạo giao diện trong component

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import { useState } from 'react'
 import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
-import './App.css'
+// import './App.css'
+import './components/Userprofile.css'
 import { Headeṛ, Sidebar, Footer } from './components/5.5 - Xuất và nhập component theo tên'
+import { Userprofile } from './components/5.6 - 3 quy tắc tạo giao diện trong component'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>chào mừng bạn đén vói react</h1>
-      < Headeṛ />
-      < Sidebar />
-      < Footer />
+      <h3>react</h3>
+      <Userprofile />
     </>
 
   )
---
+++ src/components/5.6 - 3 quy tắc tạo giao diện trong component.jsx
@@ -0,0 +1,23 @@
+function Userprofile() {
+    //logic
+    // UI-jsx
+    return (
+        <div>
+            <h3>bui viet khoa</h3>
+            <img className="thumbnail" src="https://i.pravatar.cc/150?img=69" alt="" />
+            <p>jacksonville</p>
+            <p>2000</p>
+            <h3>kỹ năng</h3>
+            <ul>
+                <li>html</li>
+                <li>css</li>
+                <li>laravel</li>
+                <li>react</li>
+            </ul>
+        </div >
+    )
+
+
+
+}
+export { Userprofile };
